0ec0a41f0e43c7894cf3778d609a004aa6611870
Михаил <pro100filya34@gmail.com>
2022-02-13 19:06:53 +0300

    my first save

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.freecreatives.com%2Fwp-content%2Fuploads%2F2016%2F04%2FBeautiful-City-Logo-Design.jpg&f=1&nofb=1'></img>
+        </header>
+    );
+}
+
+export default Header;
+
+{/* <div className='app-wrapper'>
+      <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.8yhnGRh3QVwkAJtSLD7yhwHaEK%26pid%3DApi&f=1'></img>
+       */}
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://tpverstak.ru/wp-content/uploads/2019/11/rice-plantation-1822444_1280.jpg'></img>
+      </div>
+      <div>
+        ava
+      </div>
+      <div>
+        My posts
+        <div>New post
+          <div>post 1 </div>
+          <div>post 2 </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
